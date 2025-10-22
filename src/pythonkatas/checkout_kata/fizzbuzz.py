0238c3a05a2d29a5fa19b0a97cf95b8a0964065b
alvaro.martin <alvaro.martin@radmas.com>
2025-10-22 17:17:40 +0200

    Refactor

--- src/pythonkatas/checkout_kata/fizzbuzz.py
+++ src/pythonkatas/checkout_kata/fizzbuzz.py
@@ -1,6 +1,18 @@
 def fizzbuzz(param):
     result = str(param)
-    result = "Fizz" if param % 3 == 0 else result
-    result = "Buzz" if param % 5 == 0 else result
-    result = "FizzBuzz" if param % 15 == 0 else result
+    result = rule_fizz(param, result)
+    result = rule_buzz(param, result)
+    result = rule_fizzbuzz(param, result)
     return result
+
+
+def rule_fizzbuzz(param, result: str) -> str:
+    return "FizzBuzz" if param % 15 == 0 else result
+
+
+def rule_buzz(param, result: str):
+    return "Buzz" if param % 5 == 0 else result
+
+
+def rule_fizz(param, result):
+    return "Fizz" if param % 3 == 0 else result

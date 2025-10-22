ee3e81873cdd586d6b4a0d4a5e550f053bd30936
alvaro.martin <alvaro.martin@radmas.com>
2025-10-22 14:11:34 +0200

    Refactor

--- src/pythonkatas/checkout_kata/Checkout.py
+++ src/pythonkatas/checkout_kata/Checkout.py
@@ -3,18 +3,19 @@ class Checkout:
         self.total = 0
         self.codes = list()
         self.fs = [
-            lambda: self.discounted_price_for(code="A", base_price=50, discount_amount=20, discount_per=3),
-            lambda: self.discounted_price_for(code="B", base_price=30, discount_amount=15, discount_per=2),
-            lambda: self.discounted_price_for(code="C", base_price=20, discount_amount=0, discount_per=1),
-            lambda: self.discounted_price_for(code="D", base_price=15, discount_amount=0, discount_per=1),
+            lambda: discounted_price_for(self.codes, code="A", base_price=50, discount_amount=20, discount_per=3),
+            lambda: discounted_price_for(self.codes, code="B", base_price=30, discount_amount=15, discount_per=2),
+            lambda: discounted_price_for(self.codes, code="C", base_price=20, discount_amount=0, discount_per=1),
+            lambda: discounted_price_for(self.codes, code="D", base_price=15, discount_amount=0, discount_per=1),
         ]
 
     def scan(self, code):
         self.codes.append(code)
         self.total = sum(f() for f in self.fs)
 
-    def discounted_price_for(self, code, base_price, discount_amount, discount_per) -> int:
-        scan_count = self.codes.count(code)
-        base_price = base_price * scan_count
-        discount = discount_amount * (scan_count // discount_per)
-        return base_price - discount
+
+def discounted_price_for(codes, code, base_price, discount_amount, discount_per) -> int:
+    scan_count = codes.count(code)
+    base_price = base_price * scan_count
+    discount = discount_amount * (scan_count // discount_per)
+    return base_price - discount

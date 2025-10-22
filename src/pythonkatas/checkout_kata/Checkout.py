f21c3807cbca217e73578165e3748fdc711e40cc
alvaro.martin <alvaro.martin@radmas.com>
2025-10-22 14:09:27 +0200

    Refactor

--- src/pythonkatas/checkout_kata/Checkout.py
+++ src/pythonkatas/checkout_kata/Checkout.py
@@ -5,15 +5,13 @@ class Checkout:
 
     def scan(self, code):
         self.codes.append(code)
-        self.total = 0
         fs = [
             lambda: self.discounted_price_for(code="A", base_price=50, discount_amount=20, discount_per=3),
             lambda: self.discounted_price_for(code="B", base_price=30, discount_amount=15, discount_per=2),
             lambda: self.discounted_price_for(code="C", base_price=20, discount_amount=0, discount_per=1),
             lambda: self.discounted_price_for(code="D", base_price=15, discount_amount=0, discount_per=1),
         ]
-        for f in fs:
-            self.total += f()
+        self.total = sum(f() for f in fs)
 
     def discounted_price_for(self, code, base_price, discount_amount, discount_per) -> int:
         scan_count = self.codes.count(code)

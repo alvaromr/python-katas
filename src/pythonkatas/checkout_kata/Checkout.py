00fa078afbbcdd273d3adfb5a6433b9c8d61ddd3
alvaro.martin <alvaro.martin@radmas.com>
2025-10-22 16:17:41 +0200

    Refactor

--- src/pythonkatas/checkout_kata/Checkout.py
+++ src/pythonkatas/checkout_kata/Checkout.py
@@ -1,10 +1,20 @@
-def discounted_price_rule(code, base_price, discount_amount, discount_per):
-    def discounted_price_for(codes) -> int:
-        scan_count = codes.count(code)
-        total_base = base_price * scan_count
-        discount = discount_amount * (scan_count // discount_per)
+class PriceRule:
+    def __init__(self, code, base_price, discount_amount, discount_per):
+        self.code = code
+        self.base_price = base_price
+        self.discount_amount = discount_amount
+        self.discount_per = discount_per
+
+    def apply(self, codes) -> int:
+        scan_count = codes.count(self.code)
+        total_base = self.base_price * scan_count
+        discount = self.discount_amount * (scan_count // self.discount_per)
         return total_base - discount
-    return discounted_price_for
+
+
+def discounted_price_rule(code, base_price, discount_amount, discount_per):
+    rule = PriceRule(code, base_price, discount_amount, discount_per)
+    return rule.apply
 
 class Checkout:
     def __init__(self, price_rules):

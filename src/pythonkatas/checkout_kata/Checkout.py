ef6dc865cd8df81b672a2c93ded4dbaa3ec1c0ff
alvaro.martin <alvaro.martin@radmas.com>
2025-10-22 14:48:51 +0200

    Refactor

--- src/pythonkatas/checkout_kata/Checkout.py
+++ src/pythonkatas/checkout_kata/Checkout.py
@@ -1,9 +1,16 @@
+def discounted_price_rule(code, base_price, discount_amount, discount_per):
+    def discounted_price_for(codes) -> int:
+        scan_count = codes.count(code)
+        total_base = base_price * scan_count
+        discount = discount_amount * (scan_count // discount_per)
+        return total_base - discount
+    return discounted_price_for
 
-fs = [
-    lambda codes: discounted_price_for(codes, code="A", base_price=50, discount_amount=20, discount_per=3),
-    lambda codes: discounted_price_for(codes, code="B", base_price=30, discount_amount=15, discount_per=2),
-    lambda codes: discounted_price_for(codes, code="C", base_price=20, discount_amount=0, discount_per=1),
-    lambda codes: discounted_price_for(codes, code="D", base_price=15, discount_amount=0, discount_per=1),
+price_rules = [
+    discounted_price_rule(code="A", base_price=50, discount_amount=20, discount_per=3),
+    discounted_price_rule(code="B", base_price=30, discount_amount=15, discount_per=2),
+    discounted_price_rule(code="C", base_price=20, discount_amount=0, discount_per=1),
+    discounted_price_rule(code="D", base_price=15, discount_amount=0, discount_per=1),
 ]
 
 class Checkout:
@@ -13,11 +20,7 @@ class Checkout:
 
     def scan(self, code):
         self.codes.append(code)
-        self.total = sum(f(self.codes) for f in fs)
+        self.total = sum(f(self.codes) for f in price_rules)
 
 
-def discounted_price_for(codes, code, base_price, discount_amount, discount_per) -> int:
-    scan_count = codes.count(code)
-    base_price = base_price * scan_count
-    discount = discount_amount * (scan_count // discount_per)
-    return base_price - discount
+

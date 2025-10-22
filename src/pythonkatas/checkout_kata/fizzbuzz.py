9e1567364347225bc357ab77b1006c71f9e331e7
alvaro.martin <alvaro.martin@radmas.com>
2025-10-22 18:16:30 +0200

    Refactor

--- src/pythonkatas/checkout_kata/fizzbuzz.py
+++ src/pythonkatas/checkout_kata/fizzbuzz.py
@@ -4,10 +4,15 @@ def fizzbuzz(param):
     rules = [
         lambda p, n: fizzbuzz_rule(p, divider=3, transformed="Fizz"),
         lambda p, n: fizzbuzz_rule(p, divider=5, transformed="Buzz"),
+        lambda p, n: default_rule(param, result),
     ]
     for rule in rules:
         result += rule(param, result)
-    return result if result != "" else str(param)
+    return result
+
+
+def default_rule(param, result: str) -> str:
+    return str(param) if result == "" else ""
 
 
 def fizzbuzz_rule(param: int, divider, transformed) -> str:

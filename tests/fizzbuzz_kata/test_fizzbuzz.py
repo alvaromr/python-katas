80e8fd99afe4d3ad48291de673b758bcf09a4a2e
alvaro.martin <alvaro.martin@radmas.com>
2025-10-22 17:42:40 +0200

    Red

--- tests/fizzbuzz_kata/test_fizzbuzz.py
+++ tests/fizzbuzz_kata/test_fizzbuzz.py
@@ -14,6 +14,7 @@ def test_three() -> None:
 def test_five() -> None:
     assert fizzbuzz(5) == "Buzz"
     assert fizzbuzz(10) == "Buzz"
+    assert fizzbuzz(52) == "Buzz"
 
 def test_fifteen() -> None:
     assert fizzbuzz(15) == "FizzBuzz"

eb285987ebabe2a9a9ac06d4ec8cf3107e718735
alvaro.martin <alvaro.martin@radmas.com>
2025-10-22 13:44:52 +0200

    Refactor

--- src/pythonkatas/checkout_kata/Checkout.py
+++ src/pythonkatas/checkout_kata/Checkout.py
@@ -7,20 +7,20 @@ class Checkout:
         self.codes.append(code)
         self.total = 0
 
-        a_price = self.price_a()
+        a_price = self.price_a("A")
         self.total += a_price
 
-        a_price = self.price_b()
+        a_price = self.price_b("B")
         self.total += a_price
 
-    def price_b(self) -> int:
-        scan_count = self.codes.count("B")
+    def price_b(self, code) -> int:
+        scan_count = self.codes.count(code)
         base_price = 30 * scan_count
         discount = 15 * (scan_count // 2)
         return base_price - discount
 
-    def price_a(self) -> int:
-        scan_count = self.codes.count("A")
+    def price_a(self, code) -> int:
+        scan_count = self.codes.count(code)
         base_price = 50 * scan_count
         discount = 20 * (scan_count // 3)
         return base_price - discount

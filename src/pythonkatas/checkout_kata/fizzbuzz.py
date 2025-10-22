a4648bb2410de69e7f202d222e89c10e238c823a
alvaro.martin <alvaro.martin@radmas.com>
2025-10-22 17:31:43 +0200

    Refactor

--- src/pythonkatas/checkout_kata/fizzbuzz.py
+++ src/pythonkatas/checkout_kata/fizzbuzz.py
@@ -2,14 +2,14 @@ def fizzbuzz(param):
     result = str(param)
 
     rules = [
-        lambda n: rule(n, divider=15, transformed="FizzBuzz"),
-        lambda n: rule(n, divider=3, transformed="Fizz"),
-        lambda n: rule(n, divider=5, transformed="Buzz")
+        lambda n: division_rule(n, divider=15, transformed="FizzBuzz"),
+        lambda n: division_rule(n, divider=3, transformed="Fizz"),
+        lambda n: division_rule(n, divider=5, transformed="Buzz")
     ]
-    for r in rules:
-        result = r(result)
+    for rule in rules:
+        result = rule(result)
     return result
 
 
-def rule(param: str, divider, transformed) -> str:
+def division_rule(param: str, divider, transformed) -> str:
     return transformed if param.isdigit() and int(param) % divider == 0 else param

06a6b735444f542f84b9fd62037cfdd4611bbcc0
alvaro.martin <alvaro.martin@radmas.com>
2025-10-22 17:02:23 +0200

    Refactor

--- tests/fizzbuzz_kata/test_fizzbuzz.py
+++ tests/fizzbuzz_kata/test_fizzbuzz.py
@@ -1,17 +1,13 @@
 from pythonkatas.checkout_kata.fizzbuzz import fizzbuzz
 
 
-def test_one() -> None:
+def test_number() -> None:
     assert fizzbuzz(1) == "1"
-
-def test_two() -> None:
     assert fizzbuzz(2) == "2"
+    assert fizzbuzz(4) == "4"
 
 def test_three() -> None:
     assert fizzbuzz(3) == "Fizz"
-
-def test_four() -> None:
-    assert fizzbuzz(4) == "4"
 
 def test_five() -> None:
     assert fizzbuzz(5) == "Buzz"

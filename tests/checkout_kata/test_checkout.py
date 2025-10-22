15d14f17c8822c3ff69cf54eab71eb0cd6886c86
alvaro.martin <alvaro.martin@radmas.com>
2025-10-22 12:09:58 +0200

    Refactor

--- tests/checkout_kata/test_checkout.py
+++ tests/checkout_kata/test_checkout.py
@@ -5,16 +5,11 @@ def test_no_items() -> None:
     assert 0 == price("")
 
 
-def test_scan_one_a() -> None:
+def test_scan_a() -> None:
     assert 50 == price("A")
-
-
-def test_scan_two_a() -> None:
     assert 100 == price("AA")
-
-
-def test_scan_three_a() -> None:
     assert 130 == price("AAA")
+    assert 180 == price("AAAA")
 
 
 def price(codes):

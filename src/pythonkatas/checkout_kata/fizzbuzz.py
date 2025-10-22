1f33b361d3252250f4d24d42199586cc647550ff
alvaro.martin <alvaro.martin@radmas.com>
2025-10-22 17:37:57 +0200

    Green

--- src/pythonkatas/checkout_kata/fizzbuzz.py
+++ src/pythonkatas/checkout_kata/fizzbuzz.py
@@ -4,7 +4,8 @@ def fizzbuzz(param):
     rules = [
         lambda n: division_rule(n, divider=15, transformed="FizzBuzz"),
         lambda n: division_rule(n, divider=3, transformed="Fizz"),
-        lambda n: division_rule(n, divider=5, transformed="Buzz")
+        lambda n: division_rule(n, divider=5, transformed="Buzz"),
+        lambda n: digit_rule(n, digit=3, transformed="Fizz")
     ]
     for rule in rules:
         result = rule(result)
@@ -13,3 +14,6 @@ def fizzbuzz(param):
 
 def division_rule(param: str, divider, transformed) -> str:
     return transformed if param.isdigit() and int(param) % divider == 0 else param
+
+def digit_rule(param: str, digit, transformed) -> str:
+    return transformed if param.isdigit() and str(digit) in param else param

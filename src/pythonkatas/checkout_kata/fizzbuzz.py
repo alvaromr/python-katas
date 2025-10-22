9bb45c1dda9b68179fc2786f0464d22b6af29cd3
alvaro.martin <alvaro.martin@radmas.com>
2025-10-22 17:20:59 +0200

    Refactor

--- src/pythonkatas/checkout_kata/fizzbuzz.py
+++ src/pythonkatas/checkout_kata/fizzbuzz.py
@@ -1,18 +1,18 @@
 def fizzbuzz(param):
     result = str(param)
-    result = rule_fizz(param, result)
-    result = rule_buzz(param, result)
-    result = rule_fizzbuzz(param, result)
+    result = rule_fizz(param, result, divider=3, transformed="Fizz")
+    result = rule_buzz(param, result, divider=5, transformed="Buzz")
+    result = rule_fizzbuzz(param, result, divider=15, transformed="FizzBuzz")
     return result
 
 
-def rule_fizzbuzz(param, result: str) -> str:
-    return "FizzBuzz" if param % 15 == 0 else result
+def rule_fizzbuzz(param, n: str, divider, transformed) -> str:
+    return transformed if param % divider == 0 else n
 
 
-def rule_buzz(param, result: str):
-    return "Buzz" if param % 5 == 0 else result
+def rule_buzz(param, n: str, divider, transformed):
+    return transformed if param % divider == 0 else n
 
 
-def rule_fizz(param, result):
-    return "Fizz" if param % 3 == 0 else result
+def rule_fizz(param, n, divider, transformed):
+    return transformed if param % divider == 0 else n

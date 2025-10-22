93e9697d9ee0b8cbc6301832d422a3baf8cf584e
alvaro.martin <alvaro.martin@radmas.com>
2025-10-22 13:37:37 +0200

    Green

--- src/pythonkatas/checkout_kata/Checkout.py
+++ src/pythonkatas/checkout_kata/Checkout.py
@@ -6,7 +6,12 @@ class Checkout:
 
     def scan(self, code):
         self.codes.append(code)
-        self.scan_count = self.codes.count(code)
+        self.scan_count = self.codes.count("A")
         base_price = 50 * self.scan_count
         discount = 20 * (self.scan_count // 3)
         self.total = base_price - discount
+
+        self.scan_count = self.codes.count("B")
+        base_price = 30 * self.scan_count
+        discount = 15 * (self.scan_count // 2)
+        self.total += base_price - discount

93f9799a3581558ee0071fee62b6b75dedd5b088
alvaro.martin <alvaro.martin@radmas.com>
2025-10-22 13:58:11 +0200

    Green

--- src/pythonkatas/checkout_kata/Checkout.py
+++ src/pythonkatas/checkout_kata/Checkout.py
@@ -10,6 +10,7 @@ class Checkout:
         self.total += self.discounted_price_for(code="A", base_price=50, discount_amount=20, discount_per=3)
         self.total += self.discounted_price_for(code="B", base_price=30, discount_amount=15, discount_per=2)
         self.total += self.discounted_price_for(code="C", base_price=20, discount_amount=0, discount_per=1)
+        self.total += self.discounted_price_for(code="D", base_price=15, discount_amount=0, discount_per=1)
 
     def discounted_price_for(self, code, base_price, discount_amount, discount_per) -> int:
         scan_count = self.codes.count(code)

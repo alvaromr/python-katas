0ffc2d5924a985e7202b89a4a00d5dedd0d1721b
alvaro.martin <alvaro.martin@radmas.com>
2025-10-22 13:25:23 +0200

    Green

--- src/pythonkatas/checkout_kata/Checkout.py
+++ src/pythonkatas/checkout_kata/Checkout.py
@@ -1,8 +1,8 @@
 class Checkout:
     def __init__(self):
         self.total = 0
+        self.scan_count = 0
 
     def scan(self, code):
-        self.total += 50
-        if self.total == 150:
-            self.total -= 20
+        self.scan_count += 1
+        self.total = 50 * self.scan_count - 20 * (self.scan_count // 3)
